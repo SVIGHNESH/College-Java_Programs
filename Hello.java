1c0e79764dfb80421e6341d5290eeec3aedd8fcd
Vighnesh <vighneshshukla00@gmail.com>
2025-04-11 07:28:19 +0530

    Something jsut added

--- Hello.java
+++ Hello.java
@@ -1,10 +1,19 @@
 public class Hello{
 
     public static void main(String []args){
-            System.out.println("Hello World!!");
-            System.out.println(2+3); 
+            // int a = 257;
+            // byte b = (byte)a ; //here byte methods doing the modulo on the value of the a to reach  the value of the byte value (it's limit is -128 to 127)
+            // System.out.println(b);
 
-        
+            // float f = 5.6f;
+            // int num2 = (int)f ;
+
+            // System.out.println(f);
+            // System.out.println(num2);
+            byte a = 23;
+            byte b = 20;
+            int result = a * b;
+            System.out.println(result);
     }
 
 }
