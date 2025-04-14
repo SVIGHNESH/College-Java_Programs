761ea8e2aa678a5a3bef0d7f13349ab9c68b6698
Vighnesh <vighneshshukla00@gmail.com>
2025-04-14 21:30:07 +0530

    drgre

---
+++ Control_Statements/Switch.java
@@ -0,0 +1,39 @@
+class Switch{
+    public static void main(String []args){
+        int n = 10; 
+        switch(n){
+
+            case 1 :
+                System.out.println("It is Monday.");
+                break;
+            case 2:
+                System.out.println("Is is Tuesday.");
+                break;
+            case 3:
+
+                System.out.println("Is is Wednesday.");
+                break;
+            case 4:
+                System.out.println("Is is Thursday.");
+                break;
+
+            case 5:
+                System.out.println("Is is Friday.");
+                break;
+
+            case 6:
+                System.out.println("Is is Saturday.");
+                break;
+            case 7:
+                System.out.println("Is is Sunday.");
+                break;
+            default:
+                System.out.println("Enter the Valid number.");
+                break;
+                
+
+
+
+        }
+    }
+}
