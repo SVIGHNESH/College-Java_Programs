1c8c8758f1cc7b88db831783a5f84a292fd0f704
Vighnesh <vighneshshukla00@gmail.com>
2025-04-14 20:47:22 +0530

    another approach for the checking the Greatest one

--- Control_Statements/Second.java
+++ Control_Statements/Second.java
@@ -2,29 +2,35 @@ class Second{
     public static void main(String []arr){
         int x = 60;
         int y = 700;
-        int z = 80000;
+        int z = 80;
 
-        if(x>y){
+        // if(x>y){
 
-            if(x > z){
-            System.out.println("X is the Greatest Element.");
-            }
-             else{
+        //     if(x > z){
+        //     System.out.println("X is the Greatest Element.");
+        //     }
+        //      else{
 
-            System.out.println("Z is the Greatest Element.");
-             }   
-        }
-        else{
-            if(y>z)
-            {
-                System.out.println("Y is the Greatest Element.");
+        //     System.out.println("Z is the Greatest Element.");
+        //      }   
+        // }
+        // else{
+        //     if(y>z)
+        //     {
+        //         System.out.println("Y is the Greatest Element.");
 
-            }
-            else{
-                System.out.println("Z is the Greatest Element");
-            }
+        //     }
+        //     else{
+        //         System.out.println("Z is the Greatest Element");
+        //     }
+
+        if(x>y && x>z)
+            System.out.println("X is the Greatest one here.");
+        else if (y>x && y >z)
+            System.out.println("Y is The GReatest one Here.");
+        else
+            System.out.println("Z is the Greatest one here.");
             
         }
         
     }
-}
