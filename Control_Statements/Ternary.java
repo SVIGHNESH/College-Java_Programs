1db396b35c8d49f2e688ccd7b35d5640dcb38963
Vighnesh <vighneshshukla00@gmail.com>
2025-04-14 21:30:01 +0530

    thei kae

---
+++ Control_Statements/Ternary.java
@@ -0,0 +1,18 @@
+class Ternary{
+    public static void main(String []args){
+        int n = 30;
+
+        // if(n%2 == 0)
+        //     System.out.println("Even");
+
+        // else
+        //     System.out.println("Odd");
+
+        // here 10 means even and 20 means odd number
+        int result = n%2 == 0 ? 10 : 20;
+        System.out.println(result);
+
+
+        
+    }
+}
