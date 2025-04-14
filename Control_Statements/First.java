9d8fee069d33e7e7bcea6e1539570c79245f4a3a
Vighnesh <vighneshshukla00@gmail.com>
2025-04-14 20:42:49 +0530

    Control Statements

---
+++ Control_Statements/First.java
@@ -0,0 +1,16 @@
+class First{
+    public static void main(String arr[]){
+        int x = 18 ;
+        int y =34 ; 
+        if(x>y){
+            System.out.println("X is greater than Y value at the " + x);
+        }
+        else{
+            System.out.println("Y is greater that X valued at the " + y );
+        }
+
+        
+
+
+    }
+}
---
+++ Control_Statements/Second.java
@@ -0,0 +1,30 @@
+class Second{
+    public static void main(String []arr){
+        int x = 60;
+        int y = 700;
+        int z = 80000;
+
+        if(x>y){
+
+            if(x > z){
+            System.out.println("X is the Greatest Element.");
+            }
+             else{
+
+            System.out.println("Z is the Greatest Element.");
+             }   
+        }
+        else{
+            if(y>z)
+            {
+                System.out.println("Y is the Greatest Element.");
+
+            }
+            else{
+                System.out.println("Z is the Greatest Element");
+            }
+            
+        }
+        
+    }
+}
