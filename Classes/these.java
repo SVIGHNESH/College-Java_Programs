527574c57c3bb9cae41ea7eb54e814bdc3baabba
Vighnesh <vighneshshukla00@gmail.com>
2025-04-16 21:38:50 +0530

    this keyword

---
+++ Classes/these.java
@@ -0,0 +1,14 @@
+class these{
+    int x;
+    these(int x){
+        this.x  = x;
+
+    }
+    public static void main(String []args){
+        these obj = new these(34);
+        System.out.println(obj.x);
+        int x;
+        
+
+    }
+}
