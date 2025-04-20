36a137f848541c3b69b10d108ee70c3a2b832106
Vighnesh <vighneshshukla00@gmail.com>
2025-04-20 18:01:21 +0530

    hthus

--- Classes/Demo.java
+++ Classes/Demo.java
@@ -2,11 +2,15 @@ class Calculator{
             int add(int n1,int n2)    {
                 return n1+n2;
             }
-            String add(int n1){
-             return  "PASS The Two Values. Not One";
+            int add(int n1){
+             return  ++n1;
             }
             String add(){
                     return "Enter The NUMBER to Add What I am going to do when you don't even pass the numbers.";
+            }
+            int add(int n1,int n2,int n3){
+                return n1+n2+n3;
+
             }
 }
 class Demo{
@@ -15,9 +19,9 @@ class Demo{
             Calculator obj = new Calculator();
         System.out.println(obj.add(23,23));
         System.out.println(obj.add(35,121));
-        System.out.println(obj.add(35,121));
-        System.out.println(obj.add(35,121));
-
+        System.out.println(obj.add(23));
+        System.out.println(obj.add());
+        System.out.println(obj.add(24,34,12));
     }
     
     
