83b0a1e72a0e352f45ebbbfbf277a207ea9e7ce5
Vighnesh <vighneshshukla00@gmail.com>
2025-04-21 20:21:05 +0530

    Call the Private constructor with the help of the getinstace() that is static and it return the object of the private constructor is returned.

---
+++ Concepts/CallPrivateConstructor.java
@@ -0,0 +1,16 @@
+class PrivateConstructor{
+    private PrivateConstructor(){
+
+    System.out.println("Private Constructor is called. ");
+    }
+    public static PrivateConstructor getinstance(){
+        return new PrivateConstructor();  //here the constructor of the  PrivateConstructor class is return by the getinstance function that itself is static when the obj of the PrivateCOnstructor is returned by the getinstance function a object of the class Private Constructor is made with the help of the new keyword.
+
+    }  
+}
+
+class CallPrivateConstructor{
+    public static void main(String[] args) {
+        PrivateConstructor ob = PrivateConstructor.getinstance();
+    }
+}
