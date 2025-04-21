496aa67306e19ce48f0fb3e4b112f88a8b00a6a2
Vighnesh <vighneshshukla00@gmail.com>
2025-04-21 19:58:37 +0530

    Code for the Dynamic Method Dispatch.

--- Concepts/C1.java
+++ Concepts/C1.java
@@ -14,12 +14,16 @@ class B1 extends A1{
 
 public void m3(){
     System.out.println("M3 of the class B");
-  }
-
+  } 
+  public void callParentM1() {
+    super.m1();  // Calls A1's m1()
+}
 }
 
+
+
 class C1{
-  public static void main() {
+  public static void main(String []args) {
     A1 ob = new B1();
     B1 ob1 = new B1();
     // ob = ob1 ;
@@ -27,9 +31,9 @@ class C1{
     
     ob.m1();
     ob.m2();
-    ob.m3();
+    // ob.m3();
 
-    ob1.m1();
+    ob1.callParentM1();
     ob1.m2();
     ob1.m3();
   }
