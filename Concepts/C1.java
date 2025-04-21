691d2e9f480ea72cb7b8eb384b5d9c7cdfe6cc4b
Vighnesh <vighneshshukla00@gmail.com>
2025-04-21 19:40:52 +0530

    there is definatel something going on

---
+++ Concepts/C1.java
@@ -0,0 +1,36 @@
+ class A1 {
+    public  void m1(){
+      System.out.println("M1 of the class A");
+    } 
+    public  void m2(){
+      System.out.println("M2 of the class A");
+    } 
+ }
+
+class B1 extends A1{
+  public void m1(){
+    System.out.println("M1 of the class B");
+  }
+
+public void m3(){
+    System.out.println("M3 of the class B");
+  }
+
+}
+
+class C1{
+  public static void main() {
+    A1 ob = new B1();
+    B1 ob1 = new B1();
+    // ob = ob1 ;
+
+    
+    ob.m1();
+    ob.m2();
+    ob.m3();
+
+    ob1.m1();
+    ob1.m2();
+    ob1.m3();
+  }
+}
