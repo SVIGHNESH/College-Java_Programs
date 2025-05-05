e4ff3398b8f23a530a63dabb9aa7cf00a066fe53
Vighnesh <vighneshshukla00@gmail.com>
2025-05-05 21:33:08 +0530

    Made a BRanch Named EXTRA

--- Packages/P1/Protection.java
+++ Packages/P1/Protection.java
@@ -5,12 +5,17 @@ public class Protection {
 private int n_pri = 2;
 protected int n_pro = 3;
 public int n_pub = 4;
-public Protection() {
+/*public Protection() {
     System.out.println("base constructor");
     System.out.println("n = " + n);
     System.out.println("n_pri = " + n_pri);
     System.out.println("n_pro = " + n_pro);
     System.out.println("n_pub = " + n_pub);
     }
+    */
+
+public void disp(){
+    System.out.println("Hegslgfjnsdkm");
+}
 }
 
--- QUESTIONS/TEST.java
+++ QUESTIONS/TEST.java
@@ -1,8 +1,15 @@
 package QUESTIONS;
+a
+import java.util.ArrayList;
+
 public class TEST {
 
    public static void main(String[] args) {
         MyClass.myobj.myMethod();
+        ArrayList<> name = new ArrayList<int>();
+
+        
+
         
    } 
 }
