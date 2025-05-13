7eab6438a36037fefff4885bd5f20f5bd8c704b1
Vighnesh <vighneshshukla00@gmail.com>
2025-05-13 20:40:23 +0530

    After Some Pause

--- ARRAYS/one.java
+++ ARRAYS/one.java
@@ -8,13 +8,15 @@ public class one{
         num[0] = 22;
         num[1] = 33;
         num[2] = 44;
+        num[3] = 44;
+        num[4] = 44;
 
         System.out.println(num[0]);
         System.out.println(num[5]);
         System.out.println(num[1]);
         System.out.println(num[2]);
         int arr[] = new int[4];
-        for(int i = 0;i<4;i++){
+        for(int i = 0;i<5;i++){
             System.out.print(arr[i]+ "\t" );
         }System.out.println();
 
@@ -22,7 +24,7 @@ public class one{
         arr[1] = 47;
         arr[2] = 4;
         arr[3] = 34;
-        for(int i = 0;i<4;i++){
+        for(int i = 0;i<5;i++){
             System.out.print(arr[i]+ "\t" );
         }System.out.println();
 
