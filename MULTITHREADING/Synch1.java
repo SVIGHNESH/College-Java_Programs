1db356a5def3b9cc10e5b756f3ed192865b317da
Vighnesh <vighneshshukla00@gmail.com>
2025-05-15 13:26:11 +0530

    Afsgdfgdghhf

--- MULTITHREADING/Synch1.java
+++ MULTITHREADING/Synch1.java
@@ -1,36 +1,3 @@
-// This program uses a synchronized block.
-class Callme {
-  void call(String msg) {
-    System.out.print("[" + msg);
-    try {
-      Thread.sleep(1000);
-    } catch (InterruptedException e) {
-      System.out.println("Interrupted");
-    }
-    System.out.println("]");
-  }
-}
-
-class Caller implements Runnable {
-  String msg;
-  Callme target;
-  Thread t;
-
-  public Caller(Callme targ, String s) {
-    target = targ;
-    msg = s;
-    t = new Thread(this);
-    t.start();
-  }
-
-  // synchronize calls to call()
-  public void run() {
-    synchronized (target) { // synchronized block
-      target.call(msg);
-    }
-  }
-}
-
 class Synch1 {
   public static void main(String args[]) {
     Callme target = new Callme();
