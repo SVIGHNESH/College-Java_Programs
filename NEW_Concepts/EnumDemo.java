574c50033da85a82ae8356184c1f20538a2ad79d
Vighnesh <vighneshshukla00@gmail.com>
2025-05-19 22:40:55 +0530

    qwerty

--- NEW_Concepts/EnumDemo.java
+++ NEW_Concepts/EnumDemo.java
@@ -4,11 +4,18 @@ enum Status {
 
 class EnumDemo {
   public static void main(String[] args) {
-    int i = 34;
-    Status e = Status.Success;
+    // int i = 34;
 
-    System.out.println(e);
-    System.out.println(e);
+    Status e = Status.Success;
+    Status []ee = Status.values();
+    
+      // System.out.println(ee[2]);
+    
+    for(Status s : ee){
+      System.out.println(s + " : " + s.ordinal());
+    }
+
+    // System.out.println(e);
 
   }
   }
