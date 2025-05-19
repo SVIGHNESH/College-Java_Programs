5d45662df899c43517267d9922b9dd14bb982db0
Vighnesh <vighneshshukla00@gmail.com>
2025-05-20 00:36:31 +0530

    hj;lkds

--- NEW_Concepts/Lambda_Expression_With_Functional_Interface/lambdaExpression.java
+++ NEW_Concepts/Lambda_Expression_With_Functional_Interface/lambdaExpression.java
@@ -2,15 +2,15 @@
 // package FunctionalInterface;
 // @FunctionalInterface
 interface just{
-    public void show();
+    public void show(int i );
     // public void see();
 }
 public class lambdaExpression {
     public static void main(String[] args) {
-        just ob = () -> {
-                    System.out.println("Using the lambda expression in the funcitonal interface.");
-        };
+        just ob = (i ) -> System.out.println("Using the lambda expression in the funcitonal interface." + i );
 
-        ob.show();
+        
+
+        ob.show(5);
     }
 } 
