81f3b0dabfaa4343b27250795acfd38e5c885304
Vighnesh <vighneshshukla00@gmail.com>
2025-05-19 23:24:10 +0530

    Playing with the ENUM class

--- NEW_Concepts/EnumSwitch.java
+++ NEW_Concepts/EnumSwitch.java
@@ -1,24 +1,28 @@
-enum Process{
-    Ready,Running,Waiting,Terminated;
+enum Process {
+    Ready, Running, Waiting, Terminated;
 }
+
 public class EnumSwitch {
     public static void main(String[] args) {
         Process s = Process.Terminated;
         switch (s) {
             case Ready:
-                System.out.println("The Process is in the Ready State"); 
+                System.out.println("The Process is in the Ready State");
                 break;
             case Running:
-            System.out.println("The Process is in the Running State");
-            break;
+                System.out.println("The Process is in the Running State");
+                break;
 
             case Waiting:
-            System.out.println("The Process is in the Waiting State");
-            break;
+                System.out.println("The Process is in the Waiting State");
+                break;
 
             default:
                 System.out.println("The Process has been Terminated");
                 break;
+
         }
+
+        System.out.println(s.getClass().getSuperclass());
     }
 }
