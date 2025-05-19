f812375b2753c0a479e12147dc1832c780f18ecb
Vighnesh <vighneshshukla00@gmail.com>
2025-05-19 22:49:09 +0530

    iT is th e EnUM Switches

---
+++ NEW_Concepts/EnumIf.java
@@ -0,0 +1,23 @@
+enum Sts{
+    Pending,Running,Success,Failed;
+
+}
+public class EnumIf {
+    public static void main(String[] args) {
+        Sts s = Sts.Pending;
+        if(s == Sts.Running){
+            System.out.println("IT is Running");    
+        }
+        else if(s == Sts.Pending){
+            System.out.println("Please Wait");
+
+        }
+        else if(s == Sts.Failed){
+            System.out.println("It is Not able to run");
+        }
+        else{
+            System.out.println("all right it is working");
+        }
+    }
+    
+}
