ab7a123b2cd07719e012b5cb02b8b937102aae98
Vighnesh <vighneshshukla00@gmail.com>
2025-05-20 00:04:30 +0530

    jkg

---
+++ NEW_Concepts/Lambda_Expression_With_Functional_Interface/functionalInterface.java
@@ -0,0 +1,19 @@
+package Lambda_Expression_With_Functional_Interface;
+// package FunctionalInterface;
+// @FunctionalInterface
+interface A {
+    public void show();
+    // public void see();
+}
+public class functionalInterface {
+    public static void main(String[] args) {
+        A ob = new A(){
+                @Override
+                public void show(){
+                    System.out.println("In the Show when implementing from the anonymously directly directly without implmenting the interface in the another class");
+                }
+        };
+
+        ob.show();
+    }
+}
