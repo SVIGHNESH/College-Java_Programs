d1f3d54ad793f5f6d886d06eeb00709ec595237d
Vighnesh <vighneshshukla00@gmail.com>
2025-05-21 21:32:53 +0530

    akjfldh;gjwhw: lfjsh

--- NEW_Concepts/FunctionalInterface/functionalInterface.java
+++ NEW_Concepts/FunctionalInterface/functionalInterface.java
@@ -1,15 +1,17 @@
 // package FunctionalInterface;
-// @FunctionalInterface
+@FunctionalInterface
 interface A {
     public void show();
-    // public void see();
+    //  public void see();
 }
 public class functionalInterface {
     public static void main(String[] args) {
         A ob = new A(){
                 public void show(){
                     System.out.println("In the Show when implementing from the anonymously directly directly without implmenting the interface in the another class");
+                    System.out.println("This is the mo");
                 }
+
         };
 
         ob.show();
---
+++ NEW_Concepts/scAnner/main.java
@@ -0,0 +1,10 @@
+
+
+
+package scAnner;
+
+public class main {
+    public static void main(String[] args) {
+        
+    }
+}
