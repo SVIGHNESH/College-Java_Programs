0288edd411c7d17d482371849e302fa7b4b64e0d
Vighnesh <vighneshshukla00@gmail.com>
2025-05-21 22:00:08 +0530

    knckl

--- NEW_Concepts/scAnner/main.java
+++ NEW_Concepts/scAnner/main.java
@@ -4,7 +4,9 @@
 package scAnner;
 
 public class main {
-    public static void main(String[] args) {
-        System.out.println("Hello World for mthe Main of the scAnner ");
+    public static void main(String[] args) 
+    {
+            System.out.println("now in the main of the scAnner Class");
+            
     }
 }
