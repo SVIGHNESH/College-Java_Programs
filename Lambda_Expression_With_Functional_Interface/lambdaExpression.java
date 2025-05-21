5c49c12a81bde6335f2acd4cc109eb21ce7e49d2
Vighnesh <vighneshshukla00@gmail.com>
2025-05-21 22:22:12 +0530

    Something critical happening here

---
+++ Lambda_Expression_With_Functional_Interface/lambdaExpression.java
@@ -0,0 +1,24 @@
+
+interface just{
+    public void show(int i );
+    // public void see();
+}
+class Hello implements just{
+        
+        public void show(int i){
+            System.out.println("in the show of the Hello Class");
+        }
+
+}
+public class lambdaExpression {
+    public static void main(String[] args) {
+        Hello ob = new Hello();
+        ob.show(34);
+
+        // just ob = (i ) -> System.out.println("Using the lambda expression in the funcitonal interface." + i );
+
+        
+
+        // ob.show(5);
+    }
+} 
--- NEW_Concepts/scAnner/main.java
+++ NEW_Concepts/scAnner/main.java
@@ -1,12 +1,12 @@
 
 
-
-package scAnner;
+import java.io.IOException;
 
 public class main {
-    public static void main(String[] args) 
-    {
-            System.out.println("now in the main of the scAnner Class");
-            
+    public static void main(String[] args) throws IOException{
+        System.out.print("enter a number:");
+           int num =  System.in.read();
+           System.out.println("The ASCII value for the given number is " + num);
+           System.out.println("The value for the given number is " + (num - 48));
     }
 }
