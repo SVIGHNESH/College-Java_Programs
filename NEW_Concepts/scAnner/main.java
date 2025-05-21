43b700b0cc3d391539b4e7689ba0f5ebf00572fe
Vighnesh <vighneshshukla00@gmail.com>
2025-05-21 23:04:33 +0530

    were

--- NEW_Concepts/scAnner/main.java
+++ NEW_Concepts/scAnner/main.java
@@ -1,12 +1,31 @@
+// import java.io.IOException;
+// import java.io.*;
 
-
-import java.io.IOException;
+import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args){
         System.out.print("enter a number:");
-           int num =  System.in.read();
-           System.out.println("The ASCII value for the given number is " + num);
-           System.out.println("The value for the given number is " + (num - 48));
+        //    int num =  System.in.read();
+        //    System.out.println("The ASCII value for the given number is " + num);
+        //    System.out.println("The value for the given number is " + (num - 48));
+        //
+        //this is the way to read line data from the console,files,network,db etc so we should close what we have opened;
+        //InputStreamReader in = new InputStreamReader(System.in);
+        //BufferedReader bf  = new BufferedReader(in );
+
+
+        // int num = Integer.parseInt(bf.readLine());
+        //System.out.println(num);
+
+        //bf.close();
+
+        //Using the Scanner
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num);
+
+
     }
 }
