143f9c9aa8be681d64c359b7344e681c0887e4d1
Vighnesh <vighneshshukla00@gmail.com>
2025-05-22 22:07:05 +0530

    threading again

---
+++ MULTITHREADING/DemoOfThreads.java
@@ -0,0 +1,17 @@
+class A {
+
+    public void show() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hi ");
+
+
+        }
+    }
+}
+
+public class DemoOfThreads {
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println();
+    }
+}
