01cd957e6644b1694704d84e2ec189a7a96ce800
Vighnesh <vighneshshukla00@gmail.com>
2025-05-25 21:51:21 +0530

    edfghnm,

--- NEW_Concepts/EncodeAndDecode/main.java
+++ NEW_Concepts/EncodeAndDecode/main.java
@@ -1,8 +1,21 @@
 import java.util.Base64;
+import java.util.Base64.Encoder;
+import java.util.Base64.Decoder;
 
 public class main {
     public static void main(String[] args) {
-        
-        Base64.getEncoder();
+        String credential = "Username:Password";
+
+        Encoder encoder=Base64.getEncoder();
+        String encodedStr = encoder.encodeToString(credential.getBytes());
+        System.out.println(encodedStr);
+
+
+        Decoder decoder = Base64.getDecoder();
+        byte[] decode = decoder.decode(encodedStr.getBytes());
+        String decodedStr = new String(decode);
+        System.out.println(decodedStr);
+
+
     }
 }
