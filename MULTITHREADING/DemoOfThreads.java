bf7e48a3992916c3085b1748f792be7e7f9f2a8c
Vighnesh <vighneshshukla00@gmail.com>
2025-05-27 21:56:41 +0530

    ;jkh;ljh;jkh

--- MULTITHREADING/DemoOfThreads.java
+++ MULTITHREADING/DemoOfThreads.java
@@ -1,47 +1,97 @@
-class A extends Thread{
-
-    synchronized public void run() {
-        for (int i = 0; i < 20; i++) {
-            System.out.println("Hi ");
-            // try{
-            // Thread.sleep(10);
-
-            // }
-            // catch(InterruptedException e ){
-            //     System.out.println("Thrown the Interruption");
-            // }
-
-
-        }
+class Counter{
+    int count;
+    public void increament(){
+        count++;
     }
 }
-class  B extends Thread {
+// class A extends Thread{
+//     Counter c = new Counter();
+//     public void run() {
+         
+//         for (int i = 0; i < 1000; i++) {
+//            c.increament();
 
-   synchronized public void run() {
-        for (int i = 0; i < 20; i++) {
-            System.out.println("Hello");
-            // System.out.println("");
-            // try{
-            // Thread.sleep(15);
+//             // System.out.println("Hi ");
+//             // try{
+//             // Thread.sleep(10);
 
-            // }
-            // catch(InterruptedException e ){
-            //     System.out.println("Thrown the Interruption");
-            // }
+//             // }
+//             // catch(InterruptedException e ){
+//             //     System.out.println("Thrown the Interruption");
+//             // }
+
+
+//         }
+//     }
+// }
+// class  B extends Thread {
+//     Counter c = new Counter();
+//    public void run() {
+        
+//         for (int i = 0; i < 1000; i++) {
+//             c.increament();
+//            // System.out.println("Hello");
+//             // System.out.println("");
+//             // try{
+//             // Thread.sleep(15);
+
+//             // }
+//             // catch(InterruptedException e ){
+//             //     System.out.println("Thrown the Interruption");
+//             // }
             
 
 
-        }
-    }
-}
+//         }
+//     }
+// }
 
 public class DemoOfThreads {
     public static void main(String[] args) {
-        A obj1 = new A();
-        B obj2 = new B();
 
-        obj1.start();
-        obj2.start();
+        Counter c = new Counter();
+
+
+        Runnable obj1 = () -> 
+            {
+                for(int i=0;i<1000;i++){
+                        c.increament();
+                }
+            };
+        Runnable obj2 = () -> 
+            {
+                for(int i=0;i<1000;i++){
+                        c.increament();
+                }
+                
+            };
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+        
+
+        t1.start();
+        t2.start();
+        System.out.println(c.count);
+
+
+
+
+
+
+
+
+
+
+
+        // Counter c = new Counter();
+        // A obj1 = new A();
+        // B obj2 = new B();
+        // System.out.println(c.count);
+        // obj1.start();
+        // obj2.start();
+
+        // System.out.println(c.count);
         
         
         
