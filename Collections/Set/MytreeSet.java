35b7d39a6fc6bcd9b7a53b9dc8fe241c2f65037e
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-06-07 19:19:51 +0530

    Refactor MytreeSet to use Set interface for better abstraction

--- Collections/Set/MytreeSet.java
+++ Collections/Set/MytreeSet.java
@@ -1,5 +1,24 @@
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Collection;
+import java.util.Iterator;
+
 
 
 public class MytreeSet {
-    
+        public static void main(String[] args) {
+            Collection<Integer> nms = new TreeSet<>();
+
+            nms.add(34);
+            nms.add(21);
+            nms.add(64);
+            nms.add(43);
+            // nms.add(34);
+            nms.add(4);
+
+            Iterator<Integer> values =nms.iterator();
+            while(values.hasNext())
+                System.out.println(values.next());
+            
+        }  
 }
