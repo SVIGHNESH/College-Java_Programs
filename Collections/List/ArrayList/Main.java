4b1fc18c4a295931a8185859f168bf0a97c9eb4e
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-06-07 16:27:48 +0530

    Refactor HashSet initialization and output formatting in Main.java

--- Collections/List/ArrayList/Main.java
+++ Collections/List/ArrayList/Main.java
@@ -7,18 +7,17 @@ class Main{
 
         Set<Integer> nums = new HashSet<>();
     
-        nums.add(5);
-        nums.add(3);
-        nums.add(3);
-        nums.add(2);
-        nums.add(7);
+        nums.add(53);
+        nums.add(31);
+        nums.add(22);
+        nums.add(730);
 
         // System.out.println("The value at the index 2 is "+nums.get(2));
         // System.out.println("Index value of the 2 is "+nums.indexOf(2));
 
 
-        for (int n : nums){
-            System.out.println(n);
+        for (int no : nums){
+            System.out.println(no);
         }
 
     }
