b284c195143c65ab8c6756ed0b6d1c8adb861f75
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-06-07 16:33:46 +0530

    Replace HashSet with TreeSet for ordered storage in Main.java

--- Collections/List/ArrayList/Main.java
+++ Collections/List/ArrayList/Main.java
@@ -1,11 +1,12 @@
-import java.util.HashSet;
+// import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 class Main{
     public static void main(String[] args) {
 
 
-        Set<Integer> nums = new HashSet<>();
+        Set<Integer> nums = new TreeSet<Integer>();
     
         nums.add(53);
         nums.add(31);
