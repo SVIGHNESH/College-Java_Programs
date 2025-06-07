0919d95f20a7ee0dcdac95e5d9867bda825a435a
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-06-07 19:40:22 +0530

    Add initial implementation of myhashmap with sample data

---
+++ Collections/MAP/myhashmap.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class myhashmap {
+    public static void main(String[] args) {
+        Map<String,Integer> marks = new HashMap<String,Integer>();
+
+            marks.put("Vighnesh",34);
+            marks.put("Suresh", 43);
+            marks.put("Devang", 30);
+            marks.put("Ritik", 59);
+            System.out.println(marks);
+
+
+        
+    }
+}
