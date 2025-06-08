9777ca766354f203a0f9d9455cc2a00f8bf8feec
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-06-08 21:48:00 +0530

    Implement Comparable interface in Student class and update sorting logic in Comparator1

--- Collections/Comparator1.java
+++ Collections/Comparator1.java
@@ -3,11 +3,21 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Comparator;
 
-class Student{
+class Student implements Comparable<Student>{
     int age;
     String name;
+    
 
     
+
+    public int compareTo(Student that) {
+        if(this.age>that.age)
+                    return 1;
+                else
+                    return -1;
+    }
+
+
     public Student(int age, String name) {
         this.age = age;
         this.name = name;
@@ -24,21 +34,16 @@ class Student{
 public class Comparator1 {
     public static void main(String[] args) {
 
-        Comparator<Student> com = new Comparator<Student>(){
-            public int compare(Student i,Student j){
-                if(i.age>j.age)
-                    return 1;
-                else
-                    return -1;
-
-            }
-        };
+        Comparator<Student> com =(Student i,Student j)-> i.age>j.age?1:-1;
+       
+        
         List<Student> Studs = new ArrayList<>();
         Studs.add(new Student(20, "Vighnesh"));
         Studs.add(new Student(20, "Suresh"));
         Studs.add(new Student(2, "Ritik"));
         Studs.add(new Student(16, "Devang"));
         Collections.sort(Studs,com);
+        Collections.sort(Studs);
         for(Student st: Studs){
             System.out.println(st);
         }
