354e8e8e832d860637cf18ef6cedb0b385e66714
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-06-08 20:27:56 +0530

    Add Comparator1 implementation to sort and display a list of integers

---
+++ Collections/Comparator1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Comparator1 {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+        nums.add(13);
+        nums.add(54);
+        nums.add(32);
+        nums.add(43);
+        nums.add(80);
+        Collections.sort(nums,);
+        System.out.println(nums);
+    
+    }
+}
