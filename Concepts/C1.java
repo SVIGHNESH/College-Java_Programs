59ff1e801f1e11fcb98cf8128afba6e716158b2a
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-06-11 22:06:55 +0530

    Refactor formatting and spacing in class definitions for improved readability

--- Concepts/C1.java
+++ Concepts/C1.java
@@ -1,37 +1,36 @@
- class A1 {
-    public  void m1(){
-      System.out.println("M1 of the class A");
-    } 
-    public  void m2(){
-      System.out.println("M2 of the class A");
-    } 
- }
+class A1 {
+  public void m1() {
+    System.out.println("M1 of the class A");
+  }
 
-class B1 extends A1{
-  public void m1(){
+  public void m2() {
+    System.out.println("M2 of the class A");
+  }
+}
+
+class B1 extends A1 {
+  public void m1() {
     System.out.println("M1 of the class B");
   }
 
-public void m3(){
+  public void m3() {
     System.out.println("M3 of the class B");
-  } 
+  }
+
   public void callParentM1() {
-    super.m1();  // Calls A1's m1()
+    super.m1(); // Calls A1's m1()
+  }
 }
-}
 
-
-
-class C1{
-  public static void main(String []args) {
+class C1 {
+  public static void main(String[] args) {
     A1 ob = new B1();
     B1 ob1 = new B1();
     // ob = ob1 ;
 
-    
     ob.m1();
     ob.m2();
-    // ob.m3();   //this will give the error because the m3 is not defined by the A1
+    // ob.m3(); //this will give the error because the m3 is not defined by the A1
 
     ob1.callParentM1();
     ob1.m2();
