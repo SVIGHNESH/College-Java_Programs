fe78f750eeaac09ec6291e53f7b3e0d1612fa192
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-06-20 22:49:49 +0530

    Refactor Object_Check and PrivateAbstractConstructor classes for improved structure and readability

--- Concepts/Object_Check.java
+++ Concepts/Object_Check.java
@@ -1,11 +1,15 @@
 
 public class Object_Check {
-  public static void main(String []args) {
-      Object_Check ob = new Object_Check();
-      System.out.println(ob);
-      public String  toString(){
-        return "HEll"
+
+  public String  toString(){
+        return "HEll";
 
       }
+  public static void main(String []args) {
+      Object_Check ob = new Object_Check();
+      
+      
+      System.out.println(ob);
+      System.out.println(ob instanceof Object_Check);
   }
 }
--- Concepts/PrivateAbstractConstructor.java
+++ Concepts/PrivateAbstractConstructor.java
@@ -3,58 +3,61 @@
  * We can Declare the Constructor as a Private
  */
 
-abstract class Vehicle{
-     private  Vehicle(){
-        System.out.println("Constructor of the Vehicle Class");
-      }
-     static Vehicle getInstance(){
-       return new Vehicle;
-     }
-  } 
-class car extends Vehicle{
-  car(){
+abstract class Vehicle {
+  private Vehicle() {
+    System.out.println("Constructor of the Vehicle Class");
+  }
+
+  static Vehicle getInstance() {
+    return new Vehicle();
+  }
+}
+
+class PrivateAbstractConstructor extends Vehicle {
+  PrivateAbstractConstructor() {
     Vehicle.getInstance();
     System.out.println("Constructor of the car is Called");
   }
-  public static void main(String []args) {
-      car c = new car();
-      
-  }
-}
 
-
-
-/*abstract class Vehicle{
-  // Vehicle(){
-  //     this(342);
-  //     System.out.println("abstract Class Default Constructor");
-      
-  // }
-  private Vehicle(int speed){
-
-      System.out.println("abstract Class parameterised Private Constructor" + speed );
-
-
-
-
-  }
-}
-class Car extends Vehicle{
-  Car(){
-    // super(321);
-    }
-}
-
-class PrivateAbstractConstructor{
   public static void main(String[] args) {
-    Car ob = new Car();
-    boolean yes = ob instanceof Car;
-    System.out.println(yes);
-    boolean isVehicle = ob instanceof Vehicle;
-    System.out.println(isVehicle);
+    PrivateAbstractConstructor c = new PrivateAbstractConstructor();
 
-    
   }
-} 
+}
 
-*/
+/*
+ * abstract class Vehicle{
+ * // Vehicle()
+ * // this(342);
+ * // System.out.println("abstract Class Default Constructor");
+ * 
+ * // }
+ * private Vehicle(int speed){
+ * 
+ * System.out.println("abstract Class parameterised Private Constructor" + speed
+ * );
+ * 
+ * 
+ * 
+ * 
+ * }
+ * }
+ * class Car extends Vehicle{
+ * Car(){
+ * // super(321);
+ * }
+ * }
+ * 
+ * class PrivateAbstractConstructor{
+ * public static void main(String[] args) {
+ * Car ob = new Car();
+ * boolean yes = ob instanceof Car;
+ * System.out.println(yes);
+ * boolean isVehicle = ob instanceof Vehicle;
+ * System.out.println(isVehicle);
+ * 
+ * 
+ * }
+ * }
+ * 
+ */
