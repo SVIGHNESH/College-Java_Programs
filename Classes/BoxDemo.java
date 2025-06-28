5356ef9c0cd5e0d8c4d5bd4ca08a176e6b0edd78
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-06-28 22:34:35 +0530

    alkfjkjfasjfh

--- Classes/BoxDemo.java
+++ Classes/BoxDemo.java
@@ -1,19 +1,20 @@
-class Box{
-	double height ;
-	double width;
-	double length;
+class Box {
+  double height;
+  double width;
+  double length;
 }
-class BoxDemo{
-	public static void main(String []args){
-		Box box1 = new Box();
-		box1.length = 10;
-		box1.width = 10;
-		box1.height = 10;
 
-		double volume;
+class BoxDemo {
+  public static void main(String[] args) {
+    Box box1 = new Box();
+    box1.length = 10;
+    box1.width = 10;
+    box1.height = 10;
 
-		volume = box1.length * box1.width * box1.height;
-		System.out.println("The Volume of the given BOx is");
-		System.out.println("The Volume of the given BOx is " + volume);
-	}
+    double volume;
+
+    volume = box1.length * box1.width * box1.height;
+    System.out.println("The Volume of the given BOx is");
+    System.out.println("The Volume of the given BOx is " + volume);
+  }
 }
