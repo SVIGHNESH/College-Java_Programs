541cde8611501069b0b21cce50b7768301dbe051
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-07-12 16:19:57 +0530

    Remove commented line and update comment for clarity in StreamMain class

--- NEW_Concepts/StreamAPI/StreamMain.java
+++ NEW_Concepts/StreamAPI/StreamMain.java
@@ -3,17 +3,23 @@ import java.util.List;
 import java.util.stream.Stream;
 
 class StreamMain{  
-  public static void main(String[] args) throws IllegalStateException {
+  public static void main(String[] args){
     List<Integer> names = Arrays.asList(143, 200 , 30 , 44,  5);
     Stream<Integer> data = names.stream();
+
+    Stream<Integer> newData = names.stream();
+
+    Stream<Integer> sortedNewData= newData.sorted();
+    sortedNewData.forEach(s -> System.out.println(s));
+    System.out.println("printing the newSortedarray ends here");
 
     // long count = data.count();
     // System.out.println(count);
     // Stream<Integer> sortStream = data.sorted();
     // sortStream.forEach(System.out::println);
-    Stream<Integer> filteredData = data.filter(n -> n%2 != 0);
+    Stream<Integer> filteredData = data.filter(n -> n%2 == 0);
     filteredData.forEach(System.out::println);
-    
+    //really a great way of performing the operation on the collection by using the stream API 
 
     // ystem.out.println("this is to
     // fter it is consumed once the strean can not be used");
