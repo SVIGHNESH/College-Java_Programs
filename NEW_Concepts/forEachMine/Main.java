43f60387ba8bb2d1c06e10a03fcb4ec6b14fe80a
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-07-13 22:56:41 +0530

    Update Main.java

--- NEW_Concepts/forEachMine/Main.java
+++ NEW_Concepts/forEachMine/Main.java
@@ -5,13 +5,13 @@ import java.util.function.Consumer;
 public class Main {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(34,32,3,26,4,6,4);
-        Consumer<Integer> con = new Consumer<Integer>() {
-            public void accept(Integer i){
-                    // System.out.println(i);
-                    System.out.println(i*2);
-            }
-        };
-        nums.forEach(con);
+        // Consumer<Integer> con = new Consumer<Integer>() {
+        //     public void accept(Integer i){
+        //             // System.out.println(i);
+        //             System.out.println(i*2);
+        //     }
+        // };
+        nums.forEach(n -> System.out.println(n);
         
     }
 }
