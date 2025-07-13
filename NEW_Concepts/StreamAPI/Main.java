b54931304ae87fa59db2f52b29227a92d5e345d0
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-07-13 16:26:47 +0530

    Refactor StreamAPI Main class to improve code structure and add sorting example

--- NEW_Concepts/StreamAPI/Main.java
+++ NEW_Concepts/StreamAPI/Main.java
@@ -1,23 +1,22 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-
 class Main {
+  private static Stream<Integer> stream;
+
   public static void main(String[] args) throws IllegalStateException {
-    List<Integer> names = Arrays.asList(143, 200, 30, 44, 5);
-  Stream<Integer> Names = names.stream();
-  Stream<Integer> mappStream  = Names.map(n -> n+1);
-  mappStream.forEach(n -> System.out.println(n));    
-    
-
-
+    // List<Integer> names = Arrays.asList(143, 200, 30, 44, 5);
+    // Stream<Integer> Names = names.stream();
+    // Stream<Integer> mappStream = Names.map(n -> n+1);
+    // mappStream.forEach(n -> System.out.println(n));
 
     // Stream<Integer> data = names.stream();
     // Stream<Integer> filteredData = data.filter(n -> n%2 == 0);
     // filteredData.forEach(n -> System.out.println(n));
-    
-    //  data.forEach(n -> System.out.println(n));
+
+    // data.forEach(n -> System.out.println(n));
     // long count = data.count();
 
     // System.out.println(count);
@@ -29,6 +28,15 @@ class Main {
 
     // Stream<Integer> mappedData = data.map(n -> n * 2);
     // mappedData.forEach(n -> System.out.println(n));
+
+   List<Integer> ls = Arrays.asList(3,2,4,24,2,4,56,5,7,42,53,1);
+    Stream<Integer> ins = ls.stream();
+    Stream<Integer> soStream = ins.sorted();
+    soStream.forEach(s -> System.out.println(s));
+    soStream.forEach(s -> System.out.println(s));
+    soStream.forEach(s -> System.out.println(s));
+
+    
 
   }
 }
